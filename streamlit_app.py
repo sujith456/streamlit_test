8c4f9930a1bfd29627643017498c5f4df151635e
sujith456 <37872782+sujith456@users.noreply.github.com>
2023-02-16 13:29:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
 
+import pandas as pd 
+import streamlit as st 
+from st_aggrid import AgGrid
+
+st.set_page_config(page_title="Netflix Shows", layout="wide") 
+st.title("Netlix shows analysis")
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+#load data into a DataFrame object:
+shows = pd.DataFrame(data)
+
+
+AgGrid(shows)
