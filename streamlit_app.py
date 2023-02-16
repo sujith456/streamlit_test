ced766b44e0f935eeb5e4375db9382645ffb32e4
sujith456 <37872782+sujith456@users.noreply.github.com>
2023-02-16 13:31:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import pandas as pd 
 import streamlit as st 
-#from st_aggrid import AgGrid
+from st_aggrid import AgGrid
 
 st.set_page_config(page_title="Netflix Shows", layout="wide") 
 st.title("Netlix shows analysis")
