9156b5b8f6b5bcdcc9c367d032e9f76c389da8e0
sujith456 <37872782+sujith456@users.noreply.github.com>
2023-02-16 13:31:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import pandas as pd 
 import streamlit as st 
-from st_aggrid import AgGrid
+#from st_aggrid import AgGrid
 
 st.set_page_config(page_title="Netflix Shows", layout="wide") 
 st.title("Netlix shows analysis")
@@ -15,4 +15,4 @@ data = {
 shows = pd.DataFrame(data)
 
 
-AgGrid(shows)
+st.table(shows)
